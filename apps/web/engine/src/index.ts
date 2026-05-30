6869d738ce977a2f5428f17cf26f7fbf6b058cd3
Gonzalo (LifeOS) <nicoagent85@users.noreply.github.com>
2026-05-30 01:52:21 +0000

    clean up duplicated engine folders

--- apps/web/engine/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './state.js';
-export * from './config.js';
-export * from './economy.js';
-export * from './actions.js';
-export * from './events/index.js';
-export * from './resolveTurn.js';
-export * from './rng.js';
--- apps/web/src/engine/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './state';
-export * from './config';
-export * from './economy';
-export * from './actions';
-export * from './events/index';
-export * from './resolveTurn';
-export * from './rng';
